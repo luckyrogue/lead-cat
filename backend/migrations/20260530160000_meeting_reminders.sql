ef650f36c27cd09d7bf45260e8974f3ec588ac98
Temirlan Rysdavletov <ip.rysdavletov@yahoo.com>
2026-05-30 23:12:11 +0500

    feat(reminders): meeting_reminders dedup table

---
+++ backend/migrations/20260530160000_meeting_reminders.sql
@@ -0,0 +1,11 @@
+-- +goose Up
+CREATE TABLE meeting_reminders (
+    meeting_id UUID NOT NULL REFERENCES meetings(id) ON DELETE CASCADE,
+    telegram_id BIGINT NOT NULL,
+    offset_minutes INT NOT NULL,
+    sent_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    PRIMARY KEY (meeting_id, telegram_id, offset_minutes)
+);
+
+-- +goose Down
+DROP TABLE IF EXISTS meeting_reminders;
